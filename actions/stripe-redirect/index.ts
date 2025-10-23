3d5efe85ff716ad674ef46464235243da2afaa81
Gionathan Vargas <gioanathanv@gmail.com>
2025-10-23 13:11:56 -0400

    stripe checkout

--- actions/stripe-redirect/index.ts
+++ actions/stripe-redirect/index.ts
@@ -47,7 +47,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
             price_data: {
               currency: "USD",
               product_data: {
-                name: "DadaRello Pro",
+                name: "TaskFlow Pro",
                 description: "Unlimited boards for your organization",
               },
               unit_amount: 2000,
